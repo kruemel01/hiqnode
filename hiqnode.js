b7b853edce731912694d0271c8ac969164ee46e3
Fabian Kästner <kruemel01@users.noreply.github.com>
2016-11-15 18:59:01 +0100

    Added basic tcp functionality

--- hiqnode.js
+++ hiqnode.js
@@ -1,1 +1,16 @@
 const net = require("net");
+
+var clients = [];
+
+const server = net.createServer(function (socket) {
+	socket.name = socket.remoteAddress + ":" + socket.remotePort;
+	clients.push(socket);
+
+	socket.write("Welcome " + socket.name + "\n");
+	console.log(socket.name);
+	socket.on("data", function (data) {
+		console.log(data);
+	});
+});
+
+server.listen(5000);
